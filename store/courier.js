e66822bbc385e9c857b2683bdb9b393035fa5163
d4talouh <holdentlol@gmail.com>
2021-06-20 13:36:48 +0300

    added saving dishes on first request to backend courier current order

--- store/courier.js
+++ store/courier.js
@@ -6,12 +6,20 @@ export const state = () => ({
   location: {
     latitude: 50.460569,
     longitude: 30.591311
-  }
+  },
+  order_dishes: null,
+  order_profile: null
 })
 
 export const mutations = {
   set_order(state, order) {
     state.order = order
+  },
+  set_order_dishes(state, order_dishes) {
+    state.order_dishes = order_dishes
+  },
+  set_order_profile(state, order_profile) {
+    state.order_profile = order_profile
   },
   set_courier_working(state, working) {
     state.courier_working = working
@@ -39,6 +47,12 @@ export const getters = {
   order: (state) => {
     return state.order
   },
+  order_dishes: (state) => {
+    return state.order_dishes
+  },
+  order_profile: (state) => {
+    return state.order_profile
+  },
   courier_working: (state) => {
     return state.courier_working
   },
@@ -50,6 +64,12 @@ export const getters = {
 export const actions = {
   do_set_order({commit}, order) {
     commit('set_order', order)
+  },
+  do_set_order_profile({commit}, order_profile) {
+    commit('set_order_profile', order_profile)
+  },
+  do_set_order_dishes({commit}, order_dishes) {
+    commit('set_order_dishes', order_dishes)
   },
   do_set_courier_working({commit}, working) {
     commit('set_courier_working', working)
